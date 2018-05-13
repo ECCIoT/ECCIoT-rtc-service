c576cd87d9b2612f55060594ae45ddf247e0f937
Guijie Lee <landriesnidis@yeah.net>
2018-05-14 00:30:05 +0800

    添加终端上线和离线的状态指令

--- src/main/java/team/ecciot/service/rtc/comm/handler/TerminalChannelHandler.java
+++ src/main/java/team/ecciot/service/rtc/comm/handler/TerminalChannelHandler.java
@@ -15,8 +15,11 @@ import team.ecciot.lib.args.model.impl.APIKeyVerifiedArgs;
 import team.ecciot.lib.args.model.impl.CheckDeviceIdentityArgs;
 import team.ecciot.lib.args.model.impl.CheckServerIdentityArgs;
 import team.ecciot.lib.args.model.impl.CheckTerminalIdentityArgs;
+import team.ecciot.lib.args.model.impl.DeviceStateChangedArgs;
+import team.ecciot.lib.args.model.impl.TerminalStateChangedArgs;
 import team.ecciot.lib.args.parser.ContentParser;
 import team.ecciot.service.rtc.comm.channel.TerminalChannelBox;
+import team.ecciot.service.rtc.comm.handler.callback.TerminalParseCallbackHandler;
 import team.ecciot.service.rtc.manager.ApplicationGroup;
 import team.ecciot.service.rtc.manager.ApplicationsManager;
 import team.ecciot.service.rtc.utils.ChannelUtils;
@@ -24,7 +27,7 @@ import team.ecciot.service.rtc.utils.ChannelUtils;
 public class TerminalChannelHandler extends SimpleChannelInboundHandler<String> {
 
 	//是否允许未知指令的执行
-	public static boolean isEnableUnknowCmd = false;
+	public static boolean isEnableForwardUnknowCmd = false;
 	
 	public static final HashMap<Channel, String> hmChannalApikey = new HashMap<Channel, String>();
 
@@ -42,7 +45,7 @@ public class TerminalChannelHandler extends SimpleChannelInboundHandler<String> 
 		String content = json.getString("content");
 
 		// 判断action是否有效
-		if (ContentParser.checkActionValidity(action) || isEnableUnknowCmd) {
+		if (ContentParser.checkActionValidity(action) || isEnableForwardUnknowCmd) {
 			// 判断当前Channel的身份是否已经验证完成，如果未完成则需要进行身份验证，若完成则解析具体的功能指令
 			if (!bCheckIdentityFinished) {
 				// 进行身份验证
@@ -83,6 +86,18 @@ public class TerminalChannelHandler extends SimpleChannelInboundHandler<String> 
 							JSONObject json = CmdBuilder.build(akva);
 							// 消息回执
 							ChannelUtils.sendMessage(channel, json.toJSONString());
+							
+							/*----------将终端登录信息发送给服务端----------*/
+							// 产生通知服务端新设备接入的消息
+							TerminalStateChangedArgs tsca = new TerminalStateChangedArgs();
+							tsca.setToken(args.getToken());
+							tsca.setState(true);
+							tsca.setTerminalInfo(args);
+							JSONObject json2 = CmdBuilder.build(tsca);
+							// 获取服务端的Channel
+							Channel channelServer = group.getServerChannelBox().getChannel();
+							// 将数据转发给服务端
+							ChannelUtils.sendMessage(channelServer, json2.toJSONString());
 						} else {
 							// 验证失败
 
@@ -113,37 +128,28 @@ public class TerminalChannelHandler extends SimpleChannelInboundHandler<String> 
 					// 执行到这里表示解析成功
 				} else {
 					// 对于服务端来说不存在消息转发的情况，因此代码执行到这里说明用户SDK版本与服务端不匹配(排除通信安全问题)。
+					
 				}
 			} else {
-				
-				/**
-				 * 将消息转发给服务器
-				 */
-				// 从hmChannalApikey中取得信道对应的Apikey
-				String apikey = hmChannalApikey.get(channel);
-				//根据apikey从ApplicationsManager获取ApplicationGroup
-				ApplicationGroup group = ApplicationsManager.getInstance().getApplicationGroupByApikey(apikey);
-				//获取服务端的Channel
-				Channel channelServer = group.getServerChannelBox().getChannel();
-				//将数据转发给服务端
-				ChannelUtils.sendMessage(channelServer, msg);
-				
-				/**
-				 * (!)暂不使用解析器实现拦截功能
-				 * 思路：通过解析器获取消息类型，并进行统计，存储记录
-				 */
 				// 设置指令内容配置解析器（通过设置XParseCallbackHandler的接口实现解析消息的过滤）
-//				ContentParser parser = new ContentParser(new ServerParseCallbackHandler(channel),
-//						ServerParseCallbackHandler.class);
-//				// 解析指令
-//				if (parser.parse(action, content)) {
-//					// 执行到这里表示解析成功
-//				} else {
-//					// 对于服务端来说不存在消息转发的情况，因此代码执行到这里说明用户SDK版本与服务端不匹配(排除通信安全问题)。
-//				}
+				ContentParser parser = new ContentParser(new TerminalParseCallbackHandler(channel));
+				// 解析指令
+				if (parser.parse(action, content)) {
+					/* 在回调处理器中执行完毕 */
+				} else {
+					/* 将其他指令转发 */
+					// 从hmChannalApikey中取得信道对应的Apikey
+					String apikey = hmChannalApikey.get(channel);
+					//根据apikey从ApplicationsManager获取ApplicationGroup
+					ApplicationGroup group = ApplicationsManager.getInstance().getApplicationGroupByApikey(apikey);
+					//获取服务端的Channel
+					Channel channelServer = group.getServerChannelBox().getChannel();
+					//将数据转发给服务端
+					ChannelUtils.sendMessage(channelServer, msg);
+				}
 			}
 		} else {
-			// 接收到无效的指令
+			/*接收到无效的指令且不允许转发未知的指令*/
 		}
 	}
 
--- src/main/java/team/ecciot/service/rtc/comm/handler/callback/TerminalParseCallbackHandler.java
+++ src/main/java/team/ecciot/service/rtc/comm/handler/callback/TerminalParseCallbackHandler.java
@@ -1,11 +1,9 @@
 package team.ecciot.service.rtc.comm.handler.callback;
 
 import io.netty.channel.Channel;
-import team.ecciot.lib.args.callback.ITerminal2ServerEventCallback;
-import team.ecciot.lib.args.model.impl.BindDeviceArgs;
-import team.ecciot.lib.args.model.impl.ControlItemArgs;
+import team.ecciot.lib.args.callback.ITerminal2RtcEventCallback;
 
-public class TerminalParseCallbackHandler extends BaseParseCallbackHandler implements ITerminal2ServerEventCallback{
+public class TerminalParseCallbackHandler extends BaseParseCallbackHandler implements ITerminal2RtcEventCallback{
 
 	public TerminalParseCallbackHandler(Channel channel) {
 		super(channel);
@@ -14,15 +12,5 @@ public class TerminalParseCallbackHandler extends BaseParseCallbackHandler imple
 	@Override
 	public void InvalidActionInstruction(String action, String content) {
 		System.out.println(String.format("未知的Action：%s，内容为：%s。", action, content));
-	}
-
-	@Override
-	public void Terminal_ControlItem(ControlItemArgs args) {
-		System.out.println(args);		
-	}
-
-	@Override
-	public void Terminal_BindDevice(BindDeviceArgs args) {
-		System.out.println(args);		
 	}
 }
