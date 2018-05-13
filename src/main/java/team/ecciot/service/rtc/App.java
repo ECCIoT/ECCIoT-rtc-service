1e51a33077e56c1c44f156415008ea2137ac2e69
Guijie Lee <landriesnidis@yeah.net>
2018-05-14 00:56:01 +0800

    启动器

--- src/main/java/team/ecciot/service/rtc/App.java
+++ src/main/java/team/ecciot/service/rtc/App.java
@@ -4,10 +4,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import com.alibaba.fastjson.JSONObject;
-
-import team.ecciot.lib.args.builder.CmdBuilder;
-import team.ecciot.lib.args.model.impl.CheckServerIdentityArgs;
 import team.ecciot.service.rtc.comm.listener.DeviceListener;
 import team.ecciot.service.rtc.comm.listener.ServerListener;
 import team.ecciot.service.rtc.comm.listener.TerminalListener;
@@ -22,19 +18,25 @@ public class App
         //自动快速地使用缺省Log4j环境
         BasicConfigurator.configure();
 
-        //产生一个用于测试的服务端身份校验指令
-//        CheckServerIdentityArgs csia = new CheckServerIdentityArgs();
-//        csia.setApikey("123456789abcdef");
-//        csia.setSecretkey("005117");
-//        JSONObject json = CmdBuilder.build(csia);
-//        System.out.println("产生一个用于测试的服务端身份校验指令:");
-//        System.out.println(json.toJSONString());
-//        System.out.println();
-        //{"action":"Server_CheckServerIdentity","content":{"apikey":"123456789abcdef","secretkey":"005117"}}
-        
+//        new ServerListener(19941).run();
+//        new Thread(){
+//        	public void run() {
+//        		
+//        	};
+//        }.start();
+        new Thread(){
+        	public void run() {
+        		new TerminalListener(19942).run();
+        	};
+        }.start();
+        new Thread(){
+        	public void run() {
+        		new DeviceListener(19943).run();
+        	};
+        }.start();
         new ServerListener(19941).run();
-        new TerminalListener(19942).run();
-        new DeviceListener(19943).run();
+        
+        
         
         LOGGER.info("ECCIoT RTC service has started.");
     }
